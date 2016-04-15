52fb4130ac8293b1101f4ffabf090a476814c98f
Titus Wormer <tituswormer@gmail.com>
2016-04-15 17:45:51 +0200

    Initial commit

---
+++ test.js
@@ -0,0 +1,59 @@
+/**
+ * @author Titus Wormer
+ * @copyright 2016 Titus Wormer
+ * @license MIT
+ * @module hast:util:whitespace
+ * @fileoverview Test suite for `whitespace`.
+ */
+
+'use strict';
+
+/* eslint-env node */
+
+/*
+ * Module dependencies.
+ */
+
+var test = require('tape');
+var whitespace = require('./index.js');
+
+/*
+ * Tests.
+ */
+
+test('whitespace', function (t) {
+    t.equal(whitespace(), false, 'should return `false` without node');
+
+    t.equal(whitespace({
+        'type': 'element',
+        'tagName': 'div'
+    }), false, 'should return `false` without text');
+
+    t.equal(whitespace({
+        'type': 'text',
+        'value': '\v'
+    }), false, 'should return `false` for other white-space');
+
+    t.equal(whitespace({
+        'type': 'text',
+        'value': ' \t\r\n\f'
+    }), true, 'should return `true` for inter-element white-space');
+
+    t.equal(whitespace({
+        'type': 'text'
+    }), true, 'should return `true` for `text` without value');
+
+    t.equal(
+        whitespace(' \v'),
+        false,
+        'should return `false` for a `string` of text'
+    );
+
+    t.equal(
+        whitespace(' \t\r\n\f'),
+        true,
+        'should return `true` for a `string` of inter-element white-space'
+    );
+
+    t.end();
+});
