63f87977836d6eeb785e40f6b0cac347334df0ab
Titus Wormer <tituswormer@gmail.com>
2019-05-26 09:33:38 +0200

    Refactor comments

--- index.js
+++ index.js
@@ -2,11 +2,10 @@
 
 module.exports = interElementWhiteSpace
 
-/* HTML white-space expression.
- * See <https://html.spec.whatwg.org/#space-character>. */
+// HTML white-space expression.
+// See <https://html.spec.whatwg.org/#space-character>.
 var re = /[ \t\n\f\r]/g
 
-/* Check if `node` is a inter-element white-space. */
 function interElementWhiteSpace(node) {
   var value
 
