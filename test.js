0ca900ad566d09b4302fc1b175f183a85960ec67
Titus Wormer <tituswormer@gmail.com>
2023-01-06 13:05:33 +0100

    Use Node test runner

--- test.js
+++ test.js
@@ -1,45 +1,44 @@
-import test from 'tape'
+import assert from 'node:assert/strict'
+import test from 'node:test'
 import {whitespace} from './index.js'
 
-test('whitespace', (t) => {
+test('whitespace', () => {
   // @ts-expect-error: runtime.
-  t.equal(whitespace(), false, 'should return `false` without node')
+  assert.equal(whitespace(), false, 'should return `false` without node')
 
-  t.equal(
+  assert.equal(
     whitespace({type: 'element', tagName: 'div'}),
     false,
     'should return `false` without text'
   )
 
-  t.equal(
+  assert.equal(
     whitespace({type: 'text', value: '\v'}),
     false,
     'should return `false` for other white-space'
   )
 
-  t.equal(
+  assert.equal(
     whitespace({type: 'text', value: ' \t\r\n\f'}),
     true,
     'should return `true` for inter-element white-space'
   )
 
-  t.equal(
+  assert.equal(
     whitespace({type: 'text'}),
     true,
     'should return `true` for `text` without value'
   )
 
-  t.equal(
+  assert.equal(
     whitespace(' \v'),
     false,
     'should return `false` for a `string` of text'
   )
 
-  t.equal(
+  assert.equal(
     whitespace(' \t\r\n\f'),
     true,
     'should return `true` for a `string` of inter-element white-space'
   )
-
-  t.end()
 })
