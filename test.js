c7687284ee5f3c2b6acf12132b51902d4139e600
Titus Wormer <tituswormer@gmail.com>
2023-07-31 17:48:26 +0200

    Refactor code-style

--- test.js
+++ test.js
@@ -1,42 +1,43 @@
 import assert from 'node:assert/strict'
 import test from 'node:test'
 import {whitespace} from './index.js'
-import * as mod from './index.js'
 
-test('whitespace', () => {
-  assert.deepEqual(
-    Object.keys(mod).sort(),
-    ['whitespace'],
-    'should expose the public api'
+test('whitespace', async function (t) {
+  await t.test('should expose the public api', async function () {
+    assert.deepEqual(Object.keys(await import('./index.js')).sort(), [
+      'whitespace'
+    ])
+  })
+
+  await t.test('should return `false` without text', async function () {
+    assert.equal(whitespace({type: 'comment', value: 'asd'}), false)
+  })
+
+  await t.test(
+    'should return `false` for other white-space',
+    async function () {
+      assert.equal(whitespace({type: 'text', value: '\v'}), false)
+    }
   )
 
-  assert.equal(
-    whitespace({type: 'comment', value: 'asd'}),
-    false,
-    'should return `false` without text'
+  await t.test(
+    'should return `true` for inter-element white-space',
+    async function () {
+      assert.equal(whitespace({type: 'text', value: ' \t\r\n\f'}), true)
+    }
   )
 
-  assert.equal(
-    whitespace({type: 'text', value: '\v'}),
-    false,
-    'should return `false` for other white-space'
+  await t.test(
+    'should return `false` for a `string` of text',
+    async function () {
+      assert.equal(whitespace(' \v'), false)
+    }
   )
 
-  assert.equal(
-    whitespace({type: 'text', value: ' \t\r\n\f'}),
-    true,
-    'should return `true` for inter-element white-space'
-  )
-
-  assert.equal(
-    whitespace(' \v'),
-    false,
-    'should return `false` for a `string` of text'
-  )
-
-  assert.equal(
-    whitespace(' \t\r\n\f'),
-    true,
-    'should return `true` for a `string` of inter-element white-space'
+  await t.test(
+    'should return `true` for a `string` of inter-element white-space',
+    async function () {
+      assert.equal(whitespace(' \t\r\n\f'), true)
+    }
   )
 })
